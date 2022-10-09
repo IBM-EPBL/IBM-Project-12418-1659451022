9ca03a3718c03c80a1252e75503003e241b1086d
Vijai Shree <vijaishreeseenivasagan@gmail.com>
2022-10-09 21:46:45 +0530

    Assessment

--- Prashanthi K M/Assignment2.py
+++
@@ -1,7 +0,0 @@
-import random
-temperature = random.random()*10
-humidity = random.random()*10
-while(temperature<9):
-    print("Low Temperature: "+str(temperature) + " Humidity: "+str(humidity))
-    temperature = random.random()*10
-print("High Temperature alarm trigered: "+str(temperature))
